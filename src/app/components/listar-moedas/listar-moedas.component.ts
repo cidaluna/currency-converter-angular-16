5fbf877d8b24a6529094091b30b7d5333193ba74
Cida Luna <cidalunna@hotmail.com>
2023-12-03 13:34:47 -0300

    Teste chamando tres moedas ok e realizando ajustes para tratamento de card com erro

--- src/app/components/listar-moedas/listar-moedas.component.ts
+++ src/app/components/listar-moedas/listar-moedas.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, of } from 'rxjs';
 import { Moeda } from 'src/app/models/moeda.model';
 import { MoedaService } from 'src/app/services/moeda.service';
 
@@ -14,56 +13,19 @@ export class ListarMoedasComponent implements OnInit {
   listaDeMoedas: Moeda[] = [];
   erroCard: boolean = false;
   sigla1 = 'CAD-BRL';
-  sigla2 = 'QARS-BRL';
+  sigla2 = 'ARS-BRL';
   sigla3 = 'GBP-BRL';
 
-  carregarTodas = ['CAD-BRL','ARS-BRL','GBP-BRL'];
+  tresMoedas = ['CAD-BRL','ARS-BRL','GBP-BRL'];
 
   siglaErro: any;
 
   constructor(private moedaService: MoedaService){}
 
   ngOnInit(): void{
-    this.obterMoedas(this.sigla1);
-    this.obterMoedas(this.sigla2);
-    this.obterMoedas(this.sigla3);
-  }
-
-  obterMoedas2(valor: string){
-    this.moedaService.getMoeda(valor).subscribe(
-      (data: any) => {
-        this.listaDeMoedas = Object.keys(data).map(key => data[key]);
-        // testes com console.log
-        //const chaves =  Object.keys(data);
-        //console.log('Componente: Método obterMoedas a partir dos Objetos x Chaves: ', chaves);
-        //const moedasMapeadas = chaves.map(key => data[key]);
-        //console.log('Moedas mapeadas e atribuidas a listaDeMoedas:', moedasMapeadas);
-      },
-      (error: any) => {
-        console.error('Erro ao carregar as moedas', error);
-      }
-    );
-  }
-
-  //novo
-  obterMoedas3(valor: string){
-    this.moedaService.getMoeda(valor)
-  .pipe(
-    catchError((error) => {
-      console.log("CatchError : aconteceu algo errado aqui...", error);
-      this.erroCard = true;
-      console.log(this.erroCard);
-      return of(null);
-      })
-    ).subscribe((data: any) => {
-      if(data){
-        this.listaDeMoedas = Object.keys(data).map(key => data[key]);
-        this.erroCard = false;
-      }else{
-        this.listaDeMoedas = [];
-        this.erroCard = true;
-      }
-    });
+    this.obterMoedas(this.tresMoedas);
+    //this.obterMoedas(this.sigla2);
+    //this.obterMoedas(this.sigla3);
   }
 
   //novo teste para recuperar titulo da moeda bad request erro
@@ -82,6 +44,8 @@ export class ListarMoedasComponent implements OnInit {
         console.log('Pegar o name da Moeda:', moedaName);
       },
       error: (error: any) => {
+        this.listaDeMoedas = [];
+        console.log('Lista erro', this.listaDeMoedas);
         console.error('Erro ao carregar as moedas...', error);
         this.erroCard = true;
         this.siglaErro = Object.keys(error).map(key => error[key]['name']);
@@ -94,24 +58,4 @@ export class ListarMoedasComponent implements OnInit {
   }
 
 
-/*
-      (data: any[]) => {
-        this.listaDeMoedas = Object.keys(data).map(key => data[key]);
-        this.erroCard = false;
-        // testes com console.log
-        const chaves =  Object.keys(data);
-        console.log('Componente: Método obterMoedas a partir dos Objetos x Chaves: ', chaves);
-        const moedasMapeadas = chaves.map(key => data[key]);
-        console.log('Moedas mapeadas e atribuidas a listaDeMoedas:', moedasMapeadas);
-        const moedaName = Object.keys(data).map(key => data[key]['name']);
-        console.log('Pegar o name da Moeda:', moedaName);
-      },
-      (error: any) => {
-        console.error('Erro ao carregar as moedas...', error);
-        this.erroCard = true;
-        this.siglaErro = Object.keys(error).map(key => error[key]['name']);
-      }
-    );
-  }
-*/
 }
