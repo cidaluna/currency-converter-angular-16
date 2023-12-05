aef17ee40daecdece517a79bf6eb0b8d5a2fde4b
Cida Luna <cidalunna@hotmail.com>
2023-12-04 21:07:27 -0300

    Teste cache e atualizacao com contador no navegador

--- src/app/interceptors/cache.interceptor.ts
+++ src/app/interceptors/cache.interceptor.ts
@@ -3,59 +3,65 @@ import {
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
-  HttpResponse
+  HttpResponse,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject, catchError, of, switchMap } from 'rxjs';
+import { Observable, Subject, of, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class CacheInterceptor implements HttpInterceptor {
   private _cache: Map<
-  string,
-  {
-    expireDate: Date;
-    response: HttpResponse<any>;
-  }
-> = new Map();
+    string,
+    {
+      expireDate: Date;
+      response: HttpResponse<any>;
+    }
+  > = new Map();
 
-private _refreshSubject: Subject<string> = new Subject();
-private _requestCount = 0;
-private _maxUpdates = 10;
+  private _refreshSubject: Subject<string> = new Subject();
+  private _requestCount = 0;
+  private readonly _maxUpdates = 10;
 
-constructor() {}
+  constructor() {}
 
-intercept(
-  request: HttpRequest<any>,
-  next: HttpHandler
-): Observable<HttpEvent<any>> {
-  if (request.method !== 'GET') {
-    return next.handle(request);
-  }
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (request.method !== 'GET') {
+      return next.handle(request);
+    }
 
-  if (this._requestCount >= this._maxUpdates) {
-    console.log('[CacheInterceptor] Limite máximo de solicitações atingido. Não armazenando em cache:', request.url);
-    return next.handle(request);
-  }
+    if (this._requestCount >= this._maxUpdates) {
+      console.log(
+        '[CacheInterceptor] Limite máximo de solicitações atingido. Não armazenando em cache:',
+        request.url
+      );
+      return next.handle(request);
+    }
 
-  const cachedResponse = this._cache.get(request.url);
-  console.log('[CachedResponse]:::', cachedResponse);
+    const cachedResponse = this._cache.get(request.url);
 
-
-  if (cachedResponse && cachedResponse.expireDate > new Date()) {
-    return of(cachedResponse.response);
-  }
+    if (cachedResponse && cachedResponse.expireDate > new Date()) {
+      console.log('[CacheInterceptor] Usando resposta em cache para:', request.url);
+      return of(cachedResponse.response);
+    }
 
    // Se não houver resposta em cache ou a resposta estiver expirada
    return this._sendRequest(request, next).pipe(
-    catchError(() => of(null)),
-    switchMap((response: any) => {
-      if (response) {
-        // Cache a resposta
-        const expireDate = new Date(Date.now() + 1 * 60 * 1000);  // 1 minuto em milisegundos
-        console.log('Expira em: ',expireDate);
+    catchError((error) => {
+      // Limpar o cache em caso de erro
+      this._cache.delete(request.url);
+      return throwError(error);
+    }),
+    switchMap((event) => {
+      if (event instanceof HttpResponse) {
+        const expireDate = new Date(Date.now() + 3 * 60 * 1000); // 3 minutos em milissegundos
+        console.log('[CacheInterceptor] Expira em:', expireDate);
         this._cache.set(request.url, {
           expireDate: expireDate,
-          response: response
+          response: event,
         });
 
         // Atualize o cache automaticamente após 3 minutos
@@ -64,18 +70,22 @@ intercept(
         // Verifique o limite de atualizações
         this._requestCount++;
         if (this._requestCount >= this._maxUpdates) {
+          console.log(
+            '[CacheInterceptor] Limite de atualizações atingido. Parando de emitir atualizações.'
+          );
           this._refreshSubject.complete(); // Pare de emitir atualizações
         }
       }
-      return of(response);
-    })
+      return of(event);
+    }),
+    switchMap(() => throwError(''))
   );
 }
 
-  private _sendRequest(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    return next.handle(request);
-  }
+private _sendRequest(
+  request: HttpRequest<any>,
+  next: HttpHandler
+): Observable<HttpEvent<any>> {
+  return next.handle(request);
+}
 }
